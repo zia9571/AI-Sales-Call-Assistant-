595bb075ed2ce8f5a0650b30ab80502305f6d190
zasha <72690398+zia9571@users.noreply.github.com>
2024-12-23 01:24:53 +0530

    Update env_setup.py

--- env_setup.py
+++ env_setup.py
@@ -6,6 +6,6 @@ load_dotenv()
 config = {
     "google_creds": os.getenv("google_creds"),
     "huggingface_api_key": os.getenv("huggingface_api_key"),
-    "google_sheet_id": os.getenv("google_sheet_id"),  # Add this line
+    "google_sheet_id": os.getenv("google_sheet_id"),  
     "tf": os.getenv("TF_ENABLE_ONEDNN_OPTS"),
 }
