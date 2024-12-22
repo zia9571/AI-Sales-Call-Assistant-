d4da09db8c809c3b0e20a29887de0cf611236675
zasha <72690398+zia9571@users.noreply.github.com>
2024-12-23 01:25:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Function to load environment variables from .env file
+# function to load environment variables from .env file
 def load_env_variables():
-    # Load environment variables from .env file
     load_dotenv()
 
-    # Return the loaded environment variables as a dictionary
     return {
-        'assemblyai_api_key': os.getenv('ASSEMBLYAI_API_KEY'),
         'huggingface_api_key': os.getenv('HUGGINGFACE_API_KEY'),
         'google_creds': os.getenv('GOOGLE_CREDS_PATH'),
         'google_sheet_id': os.getenv('GOOGLE_SHEET_ID')
