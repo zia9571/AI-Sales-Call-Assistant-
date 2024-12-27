26a75d9be85c3c06e4aca5cd7f90005c20d109fb
zasha <72690398+zia9571@users.noreply.github.com>
2024-12-27 11:26:16 +0530

    Update env_setup.py

--- env_setup.py
+++ env_setup.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
 config = {
     "google_creds": os.getenv("google_creds"),
     "huggingface_api_key": os.getenv("huggingface_api_key"),
-    "google_sheet_id": os.getenv("google_sheet_id"),  
-    "tf": os.getenv("TF_ENABLE_ONEDNN_OPTS"),
+    "google_sheet_id": os.getenv("google_sheet_id"), 
+    "vosk_model_path": os.getenv("vosk_model_path")
 }
